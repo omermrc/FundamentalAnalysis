ec61142b43a2d2b026b0554e49fdca660eeb5685
omermrc <Omermrc1905@hotmail.com>
2024-08-26 11:32:44 +0300

    d

--- stockuploader.py
+++ stockuploader.py
@@ -1,49 +1,356 @@
+import streamlit as st
 import json
 import os,shutil
-import pandas as pd
 from yfinance3 import YFinance3
-import schedule
-# Read the CSV file and select columns 0 and 2
+# For DataFrame
+import pandas as pd
+import numpy as np
+import datetime
+from datetime import timezone
+import yfinance as yf
+st.set_page_config(layout="wide")
+# Main title on streamlit page
+st.title('Fundamental Stock Analysis Tool')
+
+
+# Reading Blackrock's İShares csv in order to identify the stocks and their industry/sector
 df = pd.read_csv("iShares-Russell-3000-ETF_fund.csv", usecols=[0, 2])
-
-# Filter out "Cash and/or Derivatives" sector
-df = df[df.iloc[:, 1] != "Cash and/or Derivatives"]
-
-# Extract the unique sectors from the selected columns
 unique_sectors = df.iloc[:, 1].unique()
+unique_sectors = unique_sectors[unique_sectors != "Cash and/or Derivatives"]
 
-# Directory path to save JSON files
-OUTPUT_DIRECTORY = 'json_list'
 
-for filename in os.listdir(OUTPUT_DIRECTORY):
-    file_path = os.path.join(OUTPUT_DIRECTORY, filename)
+
+import streamlit as st
+
+
+# Define a function to check if a stock ticker exists
+def is_valid_ticker(ticker):
     try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
+        stock = yf.Ticker(ticker)
+        stock_info = stock.info
+        
+        # Debugging: Print stock_info to understand its structure
+        if stock_info is None:
+            print(f"Stock info for {ticker} is None")
+            return False
+        
+        print(f"Stock info for {ticker}: {stock_info}")
+
+        # Ensure stock_info is not None and check for a valid symbol
+        if 'symbol' in stock_info:
+            return stock_info['symbol'].upper() == ticker
     except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
+        # Log the error for debugging
+        print(f"Error fetching ticker {ticker}: {e}")
+    return False
 
+# Create layout with a single column for text input
+col1, col2 = st.columns([5, 5])  # Adjust proportions as needed
+with col1:
+    user_input = st.text_input("User Preferred Stocks (Enter multiple tickers separated by comma)")
 
+# Extract and capitalize stock tickers from input
+user_stocks = [ticker.strip().upper() for ticker in user_input.split(',') if ticker.strip()]
 
+# Validate each ticker and identify invalid ones
+invalid_tickers = [ticker for ticker in user_stocks if not is_valid_ticker(ticker)]
 
-for sector in unique_sectors:
-    selected_symbols = df[df.iloc[:, 1] == sector].iloc[:, 0].head(10).tolist()
+# Display validation messages
+if user_input:
+    if invalid_tickers:
+        st.warning(f"Please enter a valid symbol. Invalid tickers: {', '.join(invalid_tickers)}")
+    else:
+        st.success("All tickers are valid!")
 
-    # Display the selected symbols
-    print(f"Stock symbols in the {sector} sector:")
-    print(selected_symbols[:10])  # Display the first 10 symbols
-    selected_symbols = selected_symbols[:10]
+# Display available sectors in the sidebar
+with st.sidebar:
+    st.subheader("Available Sectors")
+    selected_sector = st.radio("Select a sector:", unique_sectors)
 
-    SYMBOLS = selected_symbols
+# Define symbols of stocks in the selected sector
+selected_symbols = df[df.iloc[:, 1] == selected_sector].iloc[:, 0].tolist()
 
-    for symbol in SYMBOLS:
-        file_name = os.path.join(OUTPUT_DIRECTORY, f'{symbol}.json')
-        data = YFinance3(symbol)
+# Combine selected symbols and user-inputted symbols
+symbols_list = list(set(selected_symbols + user_stocks))
+num_stocks = len(symbols_list)
+no_ofsymbols = len(symbols_list)
+
+if st.button("Add Selected Symbols",selected_sector):
+    folder = 'json_list'
+    
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    for user_stock in user_stocks:
+        user_stock_filename = os.path.join(folder, f'{user_stock}.json')
         
-        # Use a context manager to open the file and write the JSON data to it
-        with open(file_name, 'w') as file:
-            json.dump(data.info, file)
-        
-        print(f'Saved to {file_name}')
+        # Only attempt to fetch data if the ticker is valid
+        if is_valid_ticker(user_stock):
+            if not os.path.exists(user_stock_filename):
+                try:
+                    # Fetch data for the symbol
+                    data = YFinance3.Ticker(user_stock).info
+
+                    # Check if data is available and valid
+                    if data:
+                        # Write JSON data to file
+                        with open(user_stock_filename, 'w') as file:
+                            json.dump(data, file)
+                        st.write(f"JSON data for symbol '{user_stock}' saved to '{user_stock_filename}'.")
+                    else:
+                        st.write(f"No valid data available for symbol '{user_stock}'.")
+                except Exception as e:
+                    st.error(f"Error fetching data for {user_stock}: {e}")
+
+
+
+
+
+######  CONFIGURATION  ########
+
+# List of stock symbols we need to run fundamental analysis on - any symbol added here must have the json file
+# containing stock info from YF
+SYMBOLS = symbols_list
+DATA_PATH = 'json_list'
+
+
+
+# Dictionary to collect data to create a DF later
+data = {
+    'Symbol': [],
+    'Name': [],
+    'Industry': [],
+    'Most Recent Quarter': [],
+    'EPS (fwd)': [],
+    'P/E (fwd)': [],
+    'PEG': [],
+    'FCFY' : [],
+    'PB': [],
+    'ROE' : [],
+    'P/S (trail)': [],
+    'DPR' : [],
+    'DY' : [],
+    'CR' : [],
+    'Beta': [],
+    'Price': [],
+    '52w Low': [],
+    '52w High': [],
+    'MarketCap': [],
+    
+
+    }
+
+
+
+######  LOADS DATA FROM JSON FILES  #######
+
+def load_data(json_data):
+    data['Symbol'].append(json_data.get('symbol', np.nan))
+    data['Name'].append(json_data.get('longName', np.nan))
+    data['Industry'].append(json_data.get('industry', np.nan))
+    data['Price'].append(json_data.get('currentPrice', np.nan))
+    data['MarketCap'].append(json_data.get('marketCap', np.nan))
+    
+     # Convert Most Recent Quarter timestamp to a date object
+    most_recent_quarter = json_data.get('mostRecentQuarter', np.nan)
+    if most_recent_quarter:
+        # Use datetime.fromtimestamp with the timezone specified
+        most_recent_quarter_date = datetime.datetime.fromtimestamp(most_recent_quarter, tz=timezone.utc).date()
+    else:
+        most_recent_quarter_date = np.nan
+    data['Most Recent Quarter'].append(most_recent_quarter_date)
+
+
+
+    # Handle missing keys gracefully
+    data['EPS (fwd)'].append(json_data.get('forwardEps', np.nan))
+    data['P/E (fwd)'].append(json_data.get('forwardPE', np.nan))
+    data['PEG'].append(json_data.get('pegRatio', np.nan))
+
+    if 'freeCashflow' in json_data and 'marketCap' in json_data:
+        fcfy = (json_data['freeCashflow'] / json_data['marketCap']) * 100
+        data['FCFY'].append(round(fcfy, 2))
+    else:
+        data['FCFY'].append(np.nan)
+
+    data['PB'].append(json_data.get('priceToBook', np.nan))
+    data['ROE'].append(json_data.get('returnOnEquity', np.nan))
+    data['P/S (trail)'].append(json_data.get('priceToSalesTrailing12Months', np.nan))
+
+    data['DPR'].append(json_data.get('payoutRatio', np.nan) * 100)
+
+    data['DY'].append(json_data.get('dividendYield', 0.0))
+    data['Beta'].append(json_data.get('beta', np.nan))
+    data['CR'].append(json_data.get('currentRatio', np.nan))
+
+    data['52w Low'].append(json_data.get('fiftyTwoWeekLow', np.nan))
+    data['52w High'].append(json_data.get('fiftyTwoWeekHigh', np.nan))
+
+
+
+########  LOADS STOCK DATA FROM JSON FILES  ###########
+
+for symbol in SYMBOLS:
+    # Specify the full path to load JSON data
+    file_name = f'{DATA_PATH}/{symbol}.json'    
+    try:
+        # Open the file in read mode
+        with open(file_name, 'r') as file:
+            # Use json.load() to parse the JSON data from the file
+            load_data(json.load(file))
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found.")
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON data: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+########  CREATE DF  ###########
+# Debug: Print the length of each list in the dictionary
+for key, value in data.items():
+    print(f"Length of {key}: {len(value)}")
+
+# Check and append np.nan if any list is shorter than the others
+max_length = max(len(v) for v in data.values())
+for key, value in data.items():
+    while len(value) < max_length:
+        data[key].append(np.nan)  # Append np.nan or a suitable default value
+
+########  CREATE DF  ###########
+# Create a DF using the dictionary
+df = pd.DataFrame(data)
+
+df = df.drop_duplicates(subset='Name', keep='first')
+
+
+# Add 52 week price range
+df['52w Range'] = ((df['Price'] - df['52w Low'])/(df['52w High'] - df['52w Low']))*100
+
+
+
+######  ADD STYLES TO DF  ######
+
+def make_pretty(styler):
+    # Column formatting
+    styler.format({'EPS (fwd)': '${:.2f}', 'P/E (fwd)': '{:.2f}', 'PEG': '{:.2f}',
+                   'FCFY': '{:.2f}%', 'PB' : '{:.2f}', 'ROE' : '{:.2f}', 'P/S (trail)': '{:.2f}',
+                   'DPR': '{:.2f}%', 'DY': '{:.2f}%', 'CR' : '{:.2f}', 'Beta': '{:.2f}', '52w Low': '${:.2f}',
+                   'Price': '${:.2f}', '52w High': '${:.2f}', '52w Range': '{:.2f}%','MarketCap': '${:,.0f}'
+                  })
+
+    # Set the bar visualization
+    styler.bar(subset=['52w Range'], align="mid", color=["salmon", "cornflowerblue"])
+
+    # Grid
+    styler.set_properties(**{'border': '0.1px solid black'})
+
+    # Set background gradients
+    columns_to_gradient = ['EPS (fwd)', 'P/E (fwd)', 'PEG', 'FCFY', 'PB', 'ROE', 'P/S (trail)', 'DPR', 'DY', 'CR']
+    for col in columns_to_gradient:
+        styler.background_gradient(subset=[col], cmap='Greens')
+    
+    # No index
+    styler.hide(axis='index')
+
+    # Left text alignment for some columns
+    styler.set_properties(subset=['Symbol', 'Name', 'Industry'], **{'text-align': 'left'})
+
+    return styler
+
+
+########  ADDS TOO TIPS ########
+def populate_tt(df, tt_data, col_name):
+    stats = df[col_name].describe()
+    
+    per25 = round(stats.loc['25%'], 2)
+    per50 = round(stats.loc['50%'], 2)
+    per75 = round(stats.loc['75%'], 2)
+
+    # Get position based on the column name
+    pos = df.columns.to_list().index(col_name)
+    if col_name == 'MarketCap':
+        stats = df[col_name].describe()
+        for index, row in df.iterrows():
+            mc = row[col_name]
+            if mc == stats.loc['min']:
+                tt_data[index][pos] = 'Lowest Market Cap'
+            elif mc == stats.loc['max']:
+                tt_data[index][pos] = 'Highest Market Cap'
+            elif mc <= per25:
+                tt_data[index][pos] = '25% of companies have Market Cap under {}'.format(per25)
+            elif mc <= per50:
+                tt_data[index][pos] = '50% of companies have Market Cap under {}'.format(per50)
+            elif mc <= per75:
+                tt_data[index][pos] = '75% of companies have Market Cap under {}'.format(per75)
+            else:
+                tt_data[index][pos] = '25% of companies have Market Cap over {}'.format(per75)
+    else:
+    
+        for index, row in df.iterrows():
+            pe = row[col_name]
+            if pe == stats.loc['min']:
+                tt_data[index][pos] = 'Lowest'
+            elif pe == stats.loc['max']:
+                tt_data[index][pos] = 'Hightest'
+            elif pe <= per25:
+                tt_data[index][pos] = '25% of companies under {}'.format(per25)
+            elif pe <= per50:
+                tt_data[index][pos] = '50% of companies under {}'.format(per50)
+            elif pe <= per75:
+                tt_data[index][pos] = '75% of companies under {}'.format(per75)
+            else:
+                tt_data[index][pos] = '25% of companies over {}'.format(per75)    
+
+
+
+#######  APPLY STYLES AND TOOLTIPS ########
+
+# Initialize tool tip data - each column is set to '' for each row
+tt_data = [['' for x in range(len(df.columns))] for y in range(len(df))]
+
+# Gather tool tip data for indicators
+populate_tt(df, tt_data, 'EPS (fwd)')
+populate_tt(df, tt_data, 'P/E (fwd)')
+populate_tt(df, tt_data, 'PEG')
+populate_tt(df, tt_data, 'FCFY')
+populate_tt(df, tt_data, 'PB')
+populate_tt(df, tt_data, 'ROE')
+populate_tt(df, tt_data, 'P/S (trail)')
+populate_tt(df, tt_data, 'DPR')
+populate_tt(df, tt_data, 'DY')
+populate_tt(df, tt_data, 'CR')
+populate_tt(df, tt_data, 'MarketCap')
+
+# Create a tool tip DF
+ttips = pd.DataFrame(data=tt_data, columns=df.columns, index=df.index)
+def highlight_user_stocks(data):
+    # Initialize a blank DataFrame to collect the style information
+    style_df = pd.DataFrame(index=data.index, columns=data.columns, dtype='object')
+    
+    # Iterate through the 'Symbol' column to find user-input stocks
+    for index, symbol in enumerate(data['Symbol']):
+        if symbol in user_stocks:
+            # If the symbol is a user-input stock, set the background color for the entire row
+            style_df.iloc[index] = 'background-color: rgba(173, 216, 230, 0.5)'
+    
+    return style_df
+
+
+
+#Add table caption and styles to DF
+df.style.pipe(make_pretty).set_caption('Fundamental Indicators').set_table_styles(
+    [{'selector': 'th.col_heading', 'props': 'text-align: center'},
+    {'selector': 'caption', 'props': [('text-align', 'center'),
+                                       ('font-size', '11pt'), ('font-weight', 'bold')]}])
+# Apply the highlighting function to the DataFrame styling
+styled_df = df.style.pipe(make_pretty).apply(highlight_user_stocks, axis=None)
+
+# Display the styled DataFrame
+row_height = 39  # Adjust this value to control row height
+height = len(df) * row_height
+
+# Display the DataFrame with the calculated height
+st.dataframe(styled_df, height=height)
+
